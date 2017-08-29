c248410d345f9cbf85f8f1b9c72e94edbc64560d
dkniazevych <dkniazevych@hiqinvest.nl>
2017-08-29 15:50:09 +0200

    parse document structure by Relationship file; Sentry issues/300291552/events/6927409424/

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as file2html from 'file2html';
 import * as mime from 'file2html/lib/mime';
 import {errorsNamespace} from 'file2html/lib/errors';
+import {parseXML} from 'file2html-xml-tools/lib/sax';
 import {readArchive, Archive, ArchiveEntry, ArchiveEntrySerialization} from 'file2html-archive-tools';
 import parseCoreProps from './parse-core-props';
 import parseDocumentContent from './word/parse-document-content';
@@ -8,6 +9,8 @@ import parseDocumentStyles from './word/parse-document-styles';
 import parseDocumentRelations from './word/parse-document-relations';
 
 const documentMimeType: string = mime.lookup('.docx');
+
+// TODO: support Presentations and Spreadsheets
 const supportedMimeTypes: string[] = [documentMimeType];
 
 export default class OOXMLReader extends file2html.Reader {
@@ -16,22 +19,6 @@ export default class OOXMLReader extends file2html.Reader {
         const {byteLength} = content;
 
         return readArchive(content).then((archive: Archive) => {
-            const meta: file2html.FileMetaInformation = Object.assign({
-                fileType: file2html.FileTypes.document,
-                mimeType: '',
-                name: '',
-                size: byteLength,
-                creator: '',
-                createdAt: '',
-                modifiedAt: ''
-            }, fileInfo.meta);
-            const queue: Promise<any>[] = [];
-            const dataType: ArchiveEntrySerialization = 'string';
-            let styles: string = '';
-            let content: string = '<div></div>';
-            let isDocument: boolean = false;
-            let relations: {[key: string]: string} = {};
-
             function getInvalidFileError () {
                 const archiveTree: string = Object.keys(archive.files || {}).join(',\n');
 
@@ -40,67 +27,104 @@ export default class OOXMLReader extends file2html.Reader {
                 )) as any;
             }
 
-            if (archive.files['word/document.xml']) {
-                isDocument = true;
-                meta.fileType = file2html.FileTypes.document;
-                meta.mimeType = documentMimeType;
-            } else {
-                // TODO: support Presentations and Spreadsheets
+            const documentRelations: ArchiveEntry = archive.file('_rels/.rels');
+
+            if (!documentRelations) {
                 return getInvalidFileError();
             }
 
-            if (isDocument) {
-                const relationsFile: ArchiveEntry = archive.file('word/_rels/document.xml.rels');
+            return documentRelations.async('string').then((documentRelationsContent: string) => {
+                let corePropertiesEntryPath: string;
+                let documentEntryPath: string;
 
-                if (relationsFile) {
-                    queue.push(relationsFile.async(dataType).then((data: string) => {
+                parseXML(documentRelationsContent, {
+                    onopentag (tagName: string, attrs: {[key: string]: string}) {
+                        if (tagName === 'Relationship') {
+                            const {Type, Target} = attrs;
+
+                            if (!Target) {
+                                return;
+                            }
+
+                            if (Type.indexOf('officeDocument') > 0) {
+                                documentEntryPath = Target;
+                            } else if (Type.indexOf('core-properties') > 0) {
+                                corePropertiesEntryPath = Target;
+                            }
+                        }
+                    }
+                });
+
+                const documentEntry: ArchiveEntry = documentEntryPath && archive.file(documentEntryPath);
+
+                if (!documentEntry) {
+                    return getInvalidFileError();
+                }
+
+                const documentFilename: string = documentEntryPath.split('/').pop();
+                const meta: file2html.FileMetaInformation = Object.assign({
+                    fileType: file2html.FileTypes.document,
+                    mimeType: '',
+                    name: '',
+                    size: byteLength,
+                    creator: '',
+                    createdAt: '',
+                    modifiedAt: ''
+                }, fileInfo.meta);
+                const queue: Promise<any>[] = [];
+                const dataType: ArchiveEntrySerialization = 'string';
+                let styles: string = '';
+                let content: string = '<div></div>';
+                let relations: {[key: string]: string} = {};
+                const relationsEntry: ArchiveEntry = archive.file(`word/_rels/${ documentFilename }.rels`);
+
+                if (relationsEntry) {
+                    queue.push(relationsEntry.async(dataType).then((data: string) => {
                         return parseDocumentRelations(data, archive).then((documentRelations) => {
                             relations = documentRelations;
                         });
                     }));
                 }
-            }
 
-            return Promise.all(queue).then(() => {
-                const queue: Promise<any>[] = [];
-                let fileEntry: ArchiveEntry = archive.file('docProps/core.xml');
+                return Promise.all(queue).then(() => {
+                    const queue: Promise<any>[] = [];
+                    const coreProperties: ArchiveEntry = (
+                        corePropertiesEntryPath &&
+                        archive.file(corePropertiesEntryPath)
+                    );
 
-                if (fileEntry) {
-                    queue.push(fileEntry.async(dataType).then((data: string) => parseCoreProps(data, meta)));
-                }
+                    if (coreProperties) {
+                        queue.push(coreProperties.async(dataType).then((data: string) => parseCoreProps(data, meta)));
+                    }
 
-                if (isDocument) {
-                    fileEntry = archive.file('word/styles.xml');
+                    // is WordProcessingML
+                    if (meta.mimeType === documentMimeType) {
+                        const stylesEntry = archive.file('word/styles.xml');
 
-                    if (fileEntry) {
-                        queue.push(fileEntry.async(dataType).then((data: string) => {
-                            return parseDocumentStyles(data);
-                        }).then((documentStyles: string) => {
-                            styles += '\n' + documentStyles;
+                        if (stylesEntry) {
+                            queue.push(stylesEntry.async(dataType).then((data: string) => {
+                                return parseDocumentStyles(data);
+                            }).then((documentStyles: string) => {
+                                styles += '\n' + documentStyles;
+                            }));
+                        }
+
+                        queue.push(documentEntry.async(dataType).then((data: string) => {
+                            return parseDocumentContent(data, {
+                                relations
+                            });
+                        }).then((data: {styles: string; content: string}) => {
+                            styles += '\n' + data.styles;
+                            content = data.content;
                         }));
                     }
 
-                    fileEntry = archive.file('word/document.xml');
-
-                    if (!fileEntry) {
-                        return getInvalidFileError();
-                    }
-
-                    queue.push(fileEntry.async(dataType).then((data: string) => {
-                        return parseDocumentContent(data, {
-                            relations
-                        });
-                    }).then((data: {styles: string; content: string}) => {
-                        styles += '\n' + data.styles;
-                        content = data.content;
+                    return Promise.all(queue).then(() => new file2html.File({
+                        meta,
+                        styles: `<style>${ styles }</style>`,
+                        content
                     }));
-                }
-
-                return Promise.all(queue).then(() => new file2html.File({
-                    meta,
-                    styles: `<style>${ styles }</style>`,
-                    content
-                }));
+                });
             });
         });
     }
